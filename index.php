acdeb6b85c0ed1820d33a631e6dbd0fa85d5e2bd
plasticinw <iplasticine182@yandex.ru>
2024-12-04 03:48:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+    #получаем дату из ссылок и сохраняем в файлы и далее это закоментируем
+    #$posts_json = file_get_contents('https://jsonplaceholder.typicode.com/posts');
+    #$comments_json = file_get_contents('https://jsonplaceholder.typicode.com/comments');
+    #file_put_contents('posts.json', $posts_json);
+    #file_put_contents('comments.json', $comments_json);
+    #дёргаем дату из файлов
+    $posts_json = file_get_contents('posts.json');
+    $comments_json = file_get_contents('comments.json');
+    #декодируем её в массив для дальнейшей вставки в бд (postgreSQL)
+    $posts_decoded = json_decode($posts_json, true);
+    $comments_decoded = json_decode($comments_json, true);
+    #вводим параметры бд, в моём случае это были 'host=localhost port=5432 dbname=inlinedatabase user=postgres password=...'
+    $connect = pg_connect('host=localhost port=5432 dbname=inlinedatabase user=postgres password=Rbkkth3008');
+    #это для проверки
+    #print_r($comments_decoded[0]);
+    #print_r($comments_decoded[499]["postId"]);
+    #print_r(count($comments_decoded));
+    #print_r($comments_decoded[0]);
+    #echo count($comments_decoded);
+    for ($i = 0; $i < count($posts_decoded); $i++) {
+        #могли бы передать сразу $posts_decoded[$i], но я оплашал и в моей таблице userId это userid, из-за разницы в регистре получается ошибка
+        $userId = $posts_decoded[$i]["userId"];
+        $id = $posts_decoded[$i]["id"];
+        $title = $posts_decoded[$i]["title"];
+        $body = $posts_decoded[$i]["body"];
+        $data = array("userid" => $userId, "id" => $id, "title" => $title, "body" => $body);
+        pg_insert($connect, "posts", $data);
+    }
+    for ($i = 0; $i < count($comments_decoded); $i++) {
+        $postId = $comments_decoded[$i]["postId"];
+        $id = $comments_decoded[$i]["id"];
+        $name = $comments_decoded[$i]["name"];
+        $email = $comments_decoded[$i]["email"];
+        $body = $comments_decoded[$i]["body"];
+        $data = array("postid" => $postId, "id" => $id, "name" => $name, "email" => $email, "body" => $body);
+        pg_insert($connect, "comments", $data);
+    }
+    echo "Загружено " . count($posts_decoded) . " записей и " . count($comments_decoded) . " комментариев";
+?>
---
+++ search.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Inline</title>
+</head>
+<body>
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-7" style="justify-content: center;">
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>title</th>
+                                    <th>body</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $connect = pg_connect('host=localhost port=5432 dbname=inlinedatabase user=postgres password=Rbkkth3008');
+                                    if(isset($_GET['search'])){
+                                        $search = $_GET['search'];
+                                        if (strlen($search)>2){
+                                            $query = "SELECT posts.title, comments.body FROM posts, comments WHERE posts.id=comments.postid AND comments.body LIKE '%$search%'";
+                                            $query_run = pg_query($connect, $query);
+                                            $query_result = pg_fetch_all($query_run);
+                                            foreach($query_result as $items){
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['title']; ?></td>
+                                                    <td><?= $items['body']; ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                            <tr>
+                                                <td colspan="2">No record found</td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
